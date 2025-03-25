2ebc653b0a42b23eec0448f34a29b0dbee442689
SoftrIn6 <iamfinaid6@gmail.com>
2025-03-25 18:09:04 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyCXGA4vU-uCtORl5Kt_WmSL3Nnc_D8c-HY",
+  authDomain: "himm-bb862.firebaseapp.com",
+  projectId: "himm-bb862",
+  storageBucket: "himm-bb862.firebasestorage.app",
+  messagingSenderId: "66636429285",
+  appId: "1:66636429285:web:212e97f6d613517e98a10c",
 };
 
 // Initialize Firebase
