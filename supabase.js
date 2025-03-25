787255f07ef9b16bf8a06f9557b169548c434200
SoftrIn6 <iamfinaid6@gmail.com>
2025-03-25 17:26:37 +0200

    Update and rename supabase.js to auth.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://oxkmrajgirwxgrougrgu.supabase.co";  // Replace with your Supabase URL
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94a21yYWpnaXJ3eGdyb3Vncmd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4ODI3OTEsImV4cCI6MjA1ODQ1ODc5MX0.gdkFZuQUhjC6syfLrZepkQATeRvE0KkqO6Nhke_lYsU";          // Replace with your actual anon key
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
